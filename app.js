b0b45e3d2662df3d1b8194d49173e6aeb3eff666
jules fabien <julesfabien96@gmail.com>
2022-09-12 17:01:26 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+require('dotenv').config()
+const port = process.env.PORT || 8000;
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./server/schema/schema')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send("hello world from express");
+})
+
+app.use(bodyParser.json());
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, ()=>{
+    console.log(`server running on port: ${port}`);
+});
---
+++ server/sampleData.js
@@ -0,0 +1,30 @@
+const clients = [
+    {
+        id:"1",
+        name:"Jules Fabien",
+        email:"julesfabien@gmail.com",
+        phone:"0785126331",
+        address:"Kigali, Gasabo"
+
+    },
+    {
+        id:"2",
+        name:"John Doe",
+        email:"johndoe@gmail.com",
+        phone:"0785122331",
+        address:"Kigali, Nyarugenge"
+
+    }
+];
+
+const projects = [
+    {
+        id:"1",
+        clientId:"1",
+        status:"In Progress",
+        name:"GraphQL MERN Stack"
+
+    }
+];
+
+module.exports = {clients, projects}
